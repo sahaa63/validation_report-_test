217a113cd7311abeecd5e0812b5e2fcb65ad7c7f
sahaa63 <63608894+sahaa63@users.noreply.github.com>
2025-01-29 18:16:25 +0530

    Update validation_report.py

--- validation_report.py
+++ validation_report.py
@@ -92,7 +92,7 @@ def generate_validation_report(excel_df, pbi_df):
         validation_report[f'{measure}_PBI'] = validation_report['unique_key'].map(dict(zip(pbi_agg['unique_key'], pbi_agg[measure])))
         
     # Calculate difference (PBI - excel)
-       validation_report[f'{measure}_Diff'] = validation_report[f'{measure}_PBI'].fillna(0) - validation_report[f'{measure}_excel'].fillna(0)
+        validation_report[f'{measure}_Diff'] = validation_report[f'{measure}_PBI'].fillna(0) - validation_report[f'{measure}_excel'].fillna(0)
     # Reorder columns
     column_order = ['unique_key'] + dims + ['presence'] + \
                    [col for measure in all_measures for col in 
